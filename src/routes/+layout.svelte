fcc6c78661a516e4c9c2985cbca7d457737f49ee
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2025-04-22 13:05:25 +0800

    shadcn

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '$lib/styles/app.pcss';
+  	import { ModeWatcher } from 'mode-watcher';
+  	import { Toaster } from '../lib/components/ui/sonner';
+	let { children } = $props();
+</script>
+
+<Toaster/>
+<ModeWatcher/>
+{@render children()}
