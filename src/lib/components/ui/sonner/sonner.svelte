5110f3a81ae54feca4ad967a789074ec36bad9bc
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2025-05-03 12:59:04 +0800

    match design sonner

--- src/lib/components/ui/sonner/sonner.svelte
+++ src/lib/components/ui/sonner/sonner.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Toaster as Sonner, type ToasterProps as SonnerProps } from "svelte-sonner";
 	import { mode } from "mode-watcher";
+	import Variables from '$lib/helpers/variables.svelte';
 
 	let { ...restProps }: SonnerProps = $props();
 </script>
@@ -10,7 +11,7 @@
 	class="toaster group"
 	toastOptions={{
 		classes: {
-			toast: "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+			toast: `group toast group-[.toaster]:bg-background${Variables.transparency ? '/80 backdrop-blur-md' : ''} group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg`,
 			description: "group-[.toast]:text-muted-foreground",
 			actionButton: "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
 			cancelButton: "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
