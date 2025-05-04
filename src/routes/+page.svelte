97da441262471075e7c22fe5aef622964d86b3f9
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2025-05-04 16:24:03 +0800

    lol

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import Home from "$lib/components/pages/Home.svelte";
     import About from '$lib/components/pages/About.svelte';
-    import Footer from '$lib/components/shared/Footer.svelte';
 </script>
 
 <svelte:head>
@@ -10,4 +9,3 @@
 
 <Home/>
 <About/>
-<Footer/>
